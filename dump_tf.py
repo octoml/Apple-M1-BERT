c704ad46a97869d0e435420f960982f4e36203f2
Bing Xu <antinucleon@gmail.com>
2020-12-11 02:08:59 -0800

    asdf

--- dump_tf.py
+++ dump_tf.py
@@ -12,6 +12,12 @@ def load_keras_model(module, name, seq_len, batch_size, report_runtime=True):
     dummy_input = tf.keras.Input(
         shape=[seq_len], batch_size=batch_size, dtype="int32")
     dummy_out = model(dummy_input)  # Propagate shapes through the keras model.
+    start = time.time()
+    repeats = 50
+    for i in range(repeats):
+        dummy_out = model(dummy_input)
+    end = time.time()
+    print("Keras Runtime: %f ms." % (1000 * ((end - start) / repeats)))
     return model
 
 
@@ -24,19 +30,6 @@ def convert_to_graphdef(model, batch_size, seq_len):
     )
     frozen_func = convert_variables_to_constants_v2(model_func)
     return frozen_func.graph.as_graph_def()
-
-def run_graph_model(frozen_graph, batch_size, seq_len):
-    g = tf.graph_util.import_graph_def(frozen_graph)
-    print(g)
-    repeats = 50
-    with tf.compat.v1.Session(graph=g) as sess:
-        dummy_input = np.random.randint(0, 10000, size=[batch_size, seq_len]).astype(np.int32)
-        out = sess.run(feed_dict={"input_ids": dummy_input}, fetches=[])
-        start = time.time()
-        for i in range(repeats):
-            out = sess.run(feed_dict={"input_ids": dummy_input},  fetches=[])
-        end = time.time()
-    print("Graph Runtime: %f ms." % (1000 * ((end - start) / repeats)))
 
 def download_model(name, batch_size, seq_len):
     import transformers
